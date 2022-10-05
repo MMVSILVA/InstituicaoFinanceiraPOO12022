6b8f2467f02b32f63515df6b8a34f3b0ec577702
202020307 <202020307@TPCP04LAB0906>
2022-10-05 20:57:45 -0300

    Adicionar arquivos de projeto.

---
+++ ControleContas/Model/Conta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleContas.Model
+{
+    public class Conta
+    {
+        //private long _numero;
+        
+        public long Numero { get; private set; }
+        public decimal Saldo { get; private set; }
+        public Conta(long numero) //método construtor,com msmo nome da classe
+       
+        {
+            Numero = numero;
+        }
+
+        public void Deposito(decimal valor)
+        {
+            Saldo = valor;
+            
+        }
+        public bool Saque(decimal valor)
+        {
+            
+            if (valor <= Saldo)
+            {
+                Saldo -= valor; //saldo = saldo-valor
+                return true;
+            }
+                
+                return false;       
+        }
+        public bool Transferencia(decimal valor, Conta destino)
+        {
+            if(Saque(valor))
+            {
+                destino.Deposito(valor);
+                return true;
+            }
+            return false;
+        }
+             
+        public override string ToString()
+        {
+            return $"Conta {this.Numero}, saldo {this.Saldo}"; 
+        }
+
+    }
+}
+
---
+++ ControleContas/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ControleContas.Model;
+
+Conta conta1 = new Conta(987654321);
+Conta conta2 = new Conta(123456789);
+Conta conta3 = new Conta(654321);
+
+conta1.Deposito(1000);
+conta2.Deposito(1000);
+conta3.Deposito(234142);
+
+Console.WriteLine($"Número da conta 1 : {conta1.Numero}. ");
+Console.WriteLine($"Número da conta 2 : {conta2.Numero}. ");
+Console.WriteLine($"Número da conta 3 : {conta3.Numero}. ");
+
+Console.WriteLine($"A conta: c/c {conta1.Numero} está com o saldo de R$ {conta1.ToString()}. ");
+
+if (conta1.Saque(100))
+{
+    Console.WriteLine($"Saque efetuado com sucesso!!.");
+    Console.WriteLine($"Conta {conta1.Numero}, saldo {conta1.ToString()}");
+
+}
+else
+{
+    Console.WriteLine($"O valor do saque é maior que o saldo");
+}
+
+if (conta1.Transferencia(500, conta2))
+
+    Console.WriteLine($"Transferência efetuado com sucesso!!.");
+{
+    Console.WriteLine(conta1.ToString());
+    Console.WriteLine(conta2.ToString());
+
+}
